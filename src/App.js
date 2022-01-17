cb89f7cb341755ea25aa7bec20791693d9578ec9
marleigh <volcana@gmail.com>
2022-01-17 11:15:29 -0800

    adding in dice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import Die from "./Die";
 
 function App() {
+  const dieLength = 10;
+  
+  const [die, setDie] = React.useState(1);
+
+
   return (
-    <div className="app">
+    <main className="app">
       <h3 className="header">Tenzies</h3>
-    </div>
+      <p className="subhead">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className="die-container">
+          <Die value={1} />
+          <Die value={5} />
+      </div>
+    </main>
   );
 }
 
---
+++ src/Die.js
@@ -0,0 +1,9 @@
+export default function Die(props) {
+    return (
+        <div className="die">
+            <p className="die-value">
+                {props.value}
+            </p>
+        </div>
+    )
+}
